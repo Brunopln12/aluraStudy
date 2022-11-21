375c4cd021643f9c95153875b4f398612e0c79e8
Brunopln12 <bruno_pinho_lobo@hotmail.com>
2022-11-21 20:44:49 -0300

    AluraMidi - Aula 4 concluindo

--- Aluramidi/Projeto da aula/main.js
+++ Aluramidi/Projeto da aula/main.js
@@ -1,20 +1,18 @@
-// Seleciona todas as teclas do nosso teclado
-
 function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 };
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+// Para
 
-// enquanto
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`//template string
 
-    listaDeTeclas[contador].onclick = tocaSom;
-
-    contador = contador + 1;
-
-    console.log(contador);
+    tecla.onclick = () => {
+        tocaSom(idAudio);
+    }
 }
